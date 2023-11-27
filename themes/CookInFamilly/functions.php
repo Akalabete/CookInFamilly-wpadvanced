703a83ab1f29e76f427914fee1837024daf7c6d3
Alexandre LACOUR <100684188+Akalabete@users.noreply.github.com>
2023-11-27 17:05:39 +0100

    added acf / cpt plugin , HM taxonomies and custom fields

--- themes/CookInFamilly/functions.php
+++ themes/CookInFamilly/functions.php
@@ -124,8 +124,104 @@ function cookinfamily_settings_field_phone_number_output(){
     }
 
 
+function cookinfamily_register_custom_post_types() {
+    $labels_ingredient = array(
+        'menu_name' => __('Ingrédients', 'cookinfamily'),
+        'name_admin_bar' => __('Ingrédient', 'cookinfamily'),
+        'add_new_item' => __('Ajouter un nouvel ingrédient', 'cookinfamily'),
+        'new_item' => __('Nouvel ingrédient', 'cookinfamily'),
+        'edit_item' => __('Modifier l\'ingrédient', 'cookinfamily'),
+        );
+    $args_ingredient = array(
+        'label' => __('Ingrédients', 'cookinfamily'),
+        'description' => __('Ingrédients', 'cookinfamily'),
+        'labels' => $labels_ingredient,
+        'supports' => array('title', 'thumbnail', 'excerpt', 'editor'),
+        'hierarchical' => false,
+        'public' => true,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'menu_position' => 40,
+        'show_in_admin_bar' => true,
+        'show_in_nav_menus' => true,
+        'can_export' => true,
+        'has_archive' => true,
+        'exclude_from_search' => false,
+        'publicly_queryable' => true,
+        'capability_type' => 'post',
+        'menu_icon'  => 'dashicons-drumstick',
+         );
+     register_post_type(
+        'cif_ingredient',
+        $args_ingredient
+    );
+}
+
+function cookinfamily_register_taxonomies() {
+
+    $labels = array(
+   	 'name'          	=> __( 'Type de plat' ),
+   	 'singular_name' 	=> __( 'Type de plat' ),
+   	 'search_items'  	=> __( 'Rechercher un type de plat' ),
+   	 'all_items'     	=> __( 'Tous les types de plats' ),
+   	 'parent_item'   	=> __( 'Parent Type de plat' ),
+   	 'parent_item_colon' => __( 'Parent Type de plat:' ),
+   	 'edit_item'     	=> __( 'Modifier un type de plat' ),
+   	 'update_item'   	=> __( 'Mettre à jour un type de plat' ),
+   	 'add_new_item'  	=> __( 'Ajouter un nouveau type de plat' ),
+   	 'new_item_name' 	=> __( 'Nouveau type de plat' ),
+   	 'menu_name'     	=> __( 'Type de plat' )
+    );
+
+    $args = array(
+   	'hierarchical'  	=> true,
+   	'labels'        	=> $labels,
+   	'show_ui'       	=> true,
+   	'show_admin_column' => true,
+    'query_var'     	=> true,
+    'show_in_rest'  	=> true,
+   	'rewrite'       	=> array( 'slug' => 'type-de-plat' )
+    );
+
+    register_taxonomy('type_de_plat', array( 'recettes' ), $args);
+
+}
+
+function cookinfamily_feeding_regime_taxonomies() {
+
+    $labels = array(
+        'name' => __( 'Régime alimentaire' ),
+        'singular_name' => __( 'Régime alimentaire' ),
+        'search_items' => __( 'Rechercher un régime alimentaire particulier' ),
+        'all_items' => __( 'Tous les régimes' ),
+        'parent_item' => __( 'Parent Régime alimentaire' ),
+        'paret_item_colon' => __( 'Parant Régime alimentaire:' ),
+        'edit_item' => __( 'Modifier le régime alimentaire' ),
+        'update_item' => __( 'Mettre a jour le régime alimentaire' ),
+        'add_new_item' => __( 'Ajouter un nouveau régime alimentaire' ),
+        'new_item_name' => __( 'Nouveau Régime alimentaire' ),
+        'menu_name' => __( 'Régime alimentaire' ),
+    );
+
+    $args = array(
+        'hierarchical'  	=> true,
+        'labels'        	=> $labels,
+        'show_ui'       	=> true,
+        'show_admin_column' => true,
+        'query_var'     	=> true,
+        'show_in_rest'  	=> true,
+        'rewrite'       	=> array( 'slug' => 'feeding_regime' )
+     );
+     register_taxonomy('feeding_regime', array( 'recettes' ), $args);
+}
+
+
+add_action('init', 'cookinfamily_feeding_regime_taxonomies');
+add_action('init', 'cookinfamily_register_taxonomies');
+add_action('init', 'cookinfamily_register_custom_post_types', 11);
 
 add_action('admin_init', 'cookinfamily_settings_register');
+
 add_action('admin_menu', 'cookinfamily_add_admin_pages', 10);
 
     
